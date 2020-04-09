8f727daffc1b7efd2a5133a7e1332644d321386e
Abhishekjha002 <abhishekjhaaj619@gmail.com>
2020-04-09 08:27:27 -0700

    combination_finite_coin

--- Recursion/permutation_and_combination_with_coin/combination_finite_coin.cpp
+++ Recursion/permutation_and_combination_with_coin/combination_finite_coin.cpp
@@ -23,13 +23,37 @@ int finiteCoinCombination(vector<int>& arr, int total, string ans, int index, ve
     }
     return c;
 }
+
+//using bits -> combination with 1 coin
+int finiteCoinCombination1(vector<int>& arr, int total, string ans, int index)
+{
+    if(index == arr.size() || total == 0)
+    {
+        if(total == 0)
+        {
+            cout<<ans<<"\n";
+            return 1;
+        }
+        return 0;
+    }
+
+    int c = 0;
+    if(total - arr[index] >= 0)
+        c += finiteCoinCombination1(arr,total-arr[index],ans + to_string(arr[index]),index+1);
+    c += finiteCoinCombination1(arr,total,ans,index+1);
+
+    return c;
+
+}
+
+
 int main()
 {
 
     vector<int> arr = {2,3,5,7};
     int total = 10;
     vector<int> visited(4,0);
-    cout<<finiteCoinCombination(arr,total,"",0,visited);
-
+    //cout<<finiteCoinCombination(arr,total,"",0,visited);
+    cout<<finiteCoinCombination1(arr,total,"",0);
 
 }
