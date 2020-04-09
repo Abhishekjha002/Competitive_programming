0751385a8e83e2e1afc50e759f2f3e29a1f0e39d
Abhishekjha002 <abhishekjhaaj619@gmail.com>
2020-04-09 08:09:27 -0700

    combination infinite coin

--- Recursion/permutation_and_combination_with_coin/combination_infinite_coin.cpp
+++ Recursion/permutation_and_combination_with_coin/combination_infinite_coin.cpp
@@ -22,13 +22,34 @@ int combination_infinite(vector<int>& arr, int total, string ans, int index)
     return c;
 
 }
+//using bits - combination infinite
+int combination_infinite1(vector<int>& arr, int total, string ans, int index)
+{
+    if(total == 0 || arr.size() == index)
+    {
+        if(total == 0)
+        {
+            cout<<ans<<"\n";
+            return 1;
+        }
+        return 0;
+    }
+
+    int c = 0;
+    if(total-arr[index] >= 0)
+        c += combination_infinite1(arr,total-arr[index],ans+to_string(arr[index]),index);
+    c += combination_infinite1(arr,total,ans,index+1);
+
+    return c;
+
+}
+
 int main()
 {
 
     //denomination
     vector<int> arr = {2,3,5,7};
     int total = 10;
-    cout<<combination_infinite(arr,total,"",0);
-
+    cout<<combination_infinite1(arr,total,"",0);
 
 }
