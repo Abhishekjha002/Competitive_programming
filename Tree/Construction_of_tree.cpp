4b2fe43c5f9c00ba067ad5471a72e3604c6a8e30
Abhishekjha002 <abhishekjhaaj619@gmail.com>
2020-06-01 01:49:38 -0700

    Construction_of_tree

---
+++ Tree/Construction_of_tree.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+        int data;
+        Node* left = nullptr;
+        Node* right = nullptr;
+    
+        Node(int data)
+        {
+            this->data = data;
+        }
+        //default
+        Node()
+        {
+
+        }
+};
+
+int idx = 0;
+Node* constructTree(vector<int>& arr)
+{
+    if(arr.size() == idx || arr[idx] == -1)
+    {
+        idx++;
+        return nullptr;
+    }
+
+    Node* node = new Node(arr[idx++]);
+    node->left = constructTree(arr);
+    node->right = constructTree(arr);
+
+    return node;
+}
+
+void display(Node* node)
+{
+    if(node == nullptr)
+        return;
+
+    string str = "";
+
+    str += ((node->left != nullptr) ? to_string(node->left->data) : ".");
+    str += "<-" + to_string(node->data) + "->";
+    str += ((node->right != nullptr) ? to_string(node->right->data) : ".");
+
+    cout<<str<<"\n";
+
+    display(node->left);
+    display(node->right);
+
+}
+
+void solve()
+{
+    vector<int> arr = {10, 20, 40, -1, -1, 50, 80, -1, -1, 90, -1, -1, 30, 60, 100, -1, -1, -1, 70, 110, -1, -1, 120, -1, -1};
+    Node *root = constructTree(arr);
+    display(root);
+}
+int main()
+{
+    solve();
+    return 0;
+}
