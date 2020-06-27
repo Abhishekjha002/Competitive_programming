8a187613ef57868d524828af9eed2588e4928da7
Abhishekjha002 <abhishekjhaaj619@gmail.com>
2020-06-26 18:41:24 -0700

    Dp

--- DP/l001.cpp
+++ DP/l001.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <climits>
 using namespace std;
 
 void display(vector<int>& arr)
@@ -321,46 +322,273 @@ int climbStairs(int n) {
 // Leetcode : 746. Min Cost Climbing Stairs
 
 int minCostClimbingStairs_01(vector<int>& cost, int n, vector<int>& dp)
+{
+    if(n <= 1)
+        return dp[n] = cost[n];
+    
+    if(dp[n] != -1)
+        return dp[n];
+    
+    int ans = min(minCostClimbingStairs_01(cost,n-1,dp), minCostClimbingStairs_01(cost,n-2,dp));
+    
+    return dp[n] = ans + cost[n];
+}
+int minCostClimbingStairs_DP(vector<int>& cost, int n, vector<int>& dp)
+{
+    int N = n;
+    for(n=0;n<=N;n++)
     {
         if(n <= 1)
-            return dp[n] = cost[n];
-        
-        if(dp[n] != -1)
-            return dp[n];
-        
-        int ans = min(minCostClimbingStairs_01(cost,n-1,dp), minCostClimbingStairs_01(cost,n-2,dp));
-        
-        return dp[n] = ans + cost[n];
-    }
-    int minCostClimbingStairs_DP(vector<int>& cost, int n, vector<int>& dp)
-    {
-        int N = n;
-        for(n=0;n<=N;n++)
         {
-            if(n <= 1)
+            dp[n] = cost[n];
+            continue;
+        }
+    
+        int ans = min(dp[n-1], dp[n-2]);
+
+        dp[n] = ans + cost[n];
+    }
+    
+    return dp[N];
+}
+int minCostClimbingStairs(vector<int>& cost) {
+    
+    int size = cost.size();
+    cost.push_back(0);
+    
+    vector<int> dp(size+1,-1);
+    
+    return minCostClimbingStairs_DP(cost,size,dp);
+    
+}
+
+// Friend pairing problem : https://www.geeksforgeeks.org/friends-pairing-problem/
+
+int friends_pairing_problem(int n, vector<int>& dp)
+{
+    if(n <= 1)
+    {
+        return dp[n] = 1;
+    }
+
+    if(dp[n] != -1)
+        return dp[n];
+
+    int single = friends_pairing_problem(n-1, dp);
+    int pair = friends_pairing_problem(n-2, dp)*(n-1);
+
+    return dp[n] = single + pair;
+}
+
+int friends_pairing_problem_DP(int n, vector<int>& dp)
+{
+    int N = n;
+    for(n=0;n<=N;n++)
+    {
+        if(n <= 1)
+        {
+            dp[n] = 1;
+            continue;
+        }
+
+        int single = dp[n-1];//friends_pairing_problem(n-1, dp);
+        int pair = dp[n-2]*(n-1);//friends_pairing_problem(n-2, dp)*(n-1);
+
+        dp[n] = single + pair;
+    }
+    return dp[N];
+}
+
+//Leetcode : 64. Minimum Path Sum
+
+int minPathSum_(int sr, int sc, vector<vector<int>>& grid, vector<vector<int>>& dp)
+{
+    if(sr == grid.size() - 1 && sc == grid[0].size() - 1)
+        dp[sr][sc] = grid[sr][sc];
+    
+    if(dp[sr][sc] != -1)
+        return dp[sr][sc];
+    
+    int minCost = INT_MAX;
+    
+    if(sr+1 <= grid.size()-1)
+        minCost = min(minCost, minPathSum_(sr+1,sc,grid,dp));
+    
+    if(sc+1 <= grid[0].size()-1)
+        minCost = min(minCost, minPathSum_(sr,sc+1,grid,dp));
+    
+    return dp[sr][sc] = grid[sr][sc] + minCost;
+    
+}
+int minPathSum_DP(vector<vector<int>>& grid, vector<vector<int>>& dp)
+{
+    for(int sr=grid.size()-1;sr>=0;sr--)
+    {
+        for(int sc=grid[0].size()-1;sc>=0;sc--)
+        {
+            if(sr == grid.size() - 1 && sc == grid[0].size() - 1)
             {
-                dp[n] = cost[n];
+                dp[sr][sc] = grid[sr][sc];
                 continue;
             }
-        
-            int ans = min(dp[n-1], dp[n-2]);
 
-            dp[n] = ans + cost[n];
+            int minCost = INT_MAX;
+
+            if(sr+1 <= grid.size()-1)
+                minCost = min(minCost, dp[sr+1][sc]);
+
+            if(sc+1 <= grid[0].size()-1)
+                minCost = min(minCost, dp[sr][sc+1]);
+
+            dp[sr][sc] = grid[sr][sc] + minCost;   
         }
-        
-        return dp[N];
     }
-    int minCostClimbingStairs(vector<int>& cost) {
-        
-        int size = cost.size();
-        cost.push_back(0);
-        
-        vector<int> dp(size+1,-1);
-        
-        return minCostClimbingStairs_DP(cost,size,dp);
-        
+    return dp[0][0];
+}
+int minPathSum(vector<vector<int>>& grid) {
+    vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+    // return minPathSum_(0,0,grid,dp);
+    return minPathSum_DP(grid,dp);
+}
+
+// Gold Mine Problem : https://www.geeksforgeeks.org/gold-mine-problem/
+
+int gold_mine_problem(int sr, int sc, vector<vector<int>>& grid, vector<vector<int>>& dp)
+{
+    if(sc == grid[0].size() - 1)
+    {
+        return dp[sr][sc] = grid[sr][sc];
     }
 
+    if(dp[sr][sc] != -1)
+        return dp[sr][sc];
+        
+    int dirN[3][2] = {{-1,1},{0,1},{1,1}};
+
+    int maxCoins = 0;
+    for(int i=0;i<3;i++)
+    {
+        int x = sr + dirN[i][0];
+        int y = sc + dirN[i][1];
+        
+        if(x >= 0 && y >= 0 && x < grid.size() && y < grid[0].size())
+            maxCoins = max(maxCoins, gold_mine_problem(x, y, grid, dp));
+    }
+
+    return dp[sr][sc] = maxCoins + grid[sr][sc];
+}
+
+int gold_mine_problem_DP(vector<vector<int>>& grid, vector<vector<int>>& dp)
+{
+    for(int sr=grid.size()-1;sr>=0;sr--)
+    {
+        for(int sc=grid[0].size()-1;sc>=0;sc--)
+        {
+            if(sc == grid[0].size() - 1)
+            {
+                dp[sr][sc] = grid[sr][sc];
+                continue;
+            }
+                
+            int dirN[3][2] = {{-1,1},{0,1},{1,1}};
+
+            int maxCoins = 0;
+            for(int i=0;i<3;i++)
+            {
+                int x = sr + dirN[i][0];
+                int y = sc + dirN[i][1];
+                
+                if(x >= 0 && y >= 0 && x < grid.size() && y < grid[0].size())
+                    maxCoins = max(maxCoins, dp[x][y]);//;max(maxCoins, gold_mine_problem(x, y, grid, dp));
+            }
+
+            dp[sr][sc] = maxCoins + grid[sr][sc];
+        }
+    }
+
+    int ans = 0;
+    for(int i=0;i<grid.size();i++)
+    {
+        ans = max(ans, dp[i][0]);
+    }
+    return ans;
+}
+
+int count_of_ways(int n, int k, vector<vector<int>>& dp)
+{
+    if(k == 1 || n == k)
+        return dp[k][n] = 1;
+
+    if(dp[k][n] != 0)
+        return dp[k][n];
+
+    int newGroup = count_of_ways(n-1, k-1, dp);
+    int existingGroup = count_of_ways(n-1, k, dp)*k;
+
+    return dp[k][n] = newGroup + existingGroup;
+}
+
+int count_of_ways_DP(int n, int k, vector<vector<int>>& dp)
+{
+    int N = n;
+    int K = k;
+    for(k=1;k<=K;k++)
+    {
+        for(n=0;n<=N;n++)
+        {
+            if(n < k)
+            {
+                dp[k][n] = 0;
+                continue;
+            }
+            
+            if(k == 1 || n == k)
+            {
+                dp[k][n] = 1;
+                continue;
+            }
+
+            int newGroup = dp[k-1][n-1];//count_of_ways(n-1, k-1, dp);
+            int existingGroup = dp[k][n-1]*k;//count_of_ways(n-1, k, dp)*k;
+
+            dp[k][n] = newGroup + existingGroup;
+        }
+    }
+    return dp[K][N];
+}
+
+int count_of_ways_02(int n, int k)
+{
+    if(k == 1)
+        return 1;
+
+    if(n < 2*k)
+        return 0;
+
+    if(n == 2*k)
+        return (n-1)*k;
+
+    int newGroup = count_of_ways_02(n-2, k-1) * (n - 1);
+    int existingGroup = count_of_ways_02(n-1, k)*k;
+
+    return newGroup + existingGroup;
+}
+
+
+void set2()
+{
+    // int n = 6;
+    // vector<int> dp(n+1,-1);
+    // // cout<<friends_pairing_problem(n, dp)<<"\n";
+    // cout<<friends_pairing_problem_DP(n,dp)<<"\n";
+    // display(dp);
+    int n = 5, k = 3;
+    vector<vector<int>> dp(k+1, vector<int>(n+1,0));
+    // cout << count_of_ways(n, k, dp)<<"\n";
+    // display2D(dp);
+    // cout << count_of_ways_02(6,3);
+    cout << count_of_ways_DP(n, k, dp);
+}
 
 void pathSet()
 {
@@ -395,7 +623,8 @@ void set1()
 void solve()
 {
     // set1();
-    pathSet();
+    // pathSet();
+    set2();
 }
 
 
